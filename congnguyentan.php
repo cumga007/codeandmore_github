b8a8b5d80d0354b4ae2c11442bc82021179e7b3a
lienle <conlocnho18@gmail.com>
2014-06-25 16:27:24 +0700

    commit branch
    commit branch

--- congnguyentan.php
+++ congnguyentan.php
@@ -1,18 +1,3 @@
 <?php
-<<<<<<< HEAD
 echo "congcongnguyentan";
-=======
-echo "congnguyentan";
-<<<<<<< HEAD
-echo "conflict";
-echo "conflict";
-echo "conflict";echo "conflict";
-echo "conflict";
-echo "again";
-echo "conflict";
-echo "conflict";echo "conflict";
->>>>>>> origin/master
-=======
-echo 'chonguyenvan edited.';
->>>>>>> 9c16874f5d8bf031737e1b9d12a832c6bc8cc5bf
 ?>
